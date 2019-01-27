8463e3b508ab492621492c77337b887be09e5615
arbaz khan18 <arbazkhan18@protonmail.com>
2019-01-27 22:08:04 +0530

    developed reactive forms component

---
+++ src/app/Forms/reactive-form/reactive-form.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-form',
+  templateUrl: './reactive-form.component.html',
+  styleUrls: ['./reactive-form.component.scss']
+})
+export class ReactiveFormComponent implements OnInit {
+
+  rForm: FormGroup
+  post: any
+  name: string = ''
+  description: string = ''
+
+  constructor( private fb: FormBuilder) { 
+    this.rForm = fb.group({
+      'name': [null, Validators.required],
+      'description': [null, Validators.compose([ Validators.required, Validators.minLength(3), Validators.maxLength(100) ]) ]
+    })
+  }
+
+  ngOnInit() {
+  }
+
+  onAddpost( value: any){
+    this.name = value.name
+    this.description = value.description
+  }
+
+  goBack(){
+    this.name = ""
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TemplateFormComponent } from './Forms/template-form/template-form.component';
+import { ReactiveFormComponent } from './Forms/reactive-form/reactive-form.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import {MatButtonModule, MatCheckboxModule, MatToolbarModule, MatCardModule} from '@angular/material';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TemplateFormComponent,
+    ReactiveFormComponent,
+    HeaderComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatInputModule, MatButtonModule, MatCheckboxModule, MatToolbarModule, MatCardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
