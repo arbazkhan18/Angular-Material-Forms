6da0dc96e2415482625dfccdfd63c462e4ead3d8
arbaz khan18 <arbazkhan18@protonmail.com>
2019-01-29 22:24:56 +0530

    added login pop up dialog

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TemplateFormComponent } from './Forms/template-form/template-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'TemplateForm',
+    component: TemplateFormComponent,
+    data: { title: 'ruck fules' }
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
